be516d6420182d4ceb45f709494b517b8407429c
Vadim <vadimmalakhovski@hotmail.com>
2018-04-20 15:04:32 +0000

    heroku youtube tutorial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap.min
-//= require jquery
+
+//= require jquery.3.2.1.min
 //= require jquery_ujs
+//= require bootstrap.min
 //= require turbolinks
 //= require_tree .
 //= require highcharts
