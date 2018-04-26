ad2693b0b644b8c997fb11c0e4a6587888075b46
Vadim <vadimmalakhovski@hotmail.com>
2018-04-26 19:09:11 +0000

    heroku final errors

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,13 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( animate.min.css )
+Rails.application.config.assets.precompile += %w( light-bootstrap-dashboard.css )
+Rails.application.config.assets.precompile += %w( pe-icon-7-stroke.css )
+
+Rails.application.config.assets.precompile += %w( jquery.3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( chartist.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap-notify.js )
+Rails.application.config.assets.precompile += %w( light-bootstrap-dashboard.js )
