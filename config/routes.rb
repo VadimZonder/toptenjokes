caa5dd547932b0903293e7b4aacc4af7d05209ee
Vadim <vadimmalakhovski@hotmail.com>
2018-04-26 15:57:22 +0000

    heroku final

--- config/routes.rb
+++ config/routes.rb
@@ -17,17 +17,6 @@ post '/search' => 'students#search'
 
   get 'sessions/destroy'
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
 
   
   resources :sessions, only: [:new, :create, :destroy]
@@ -40,31 +29,10 @@ post '/search' => 'students#search'
    root 'static_pages#home'
      get '/about' => 'static_pages#about'
  
-  #
-  #devise_for :users, path: 'users'
-# eg. http://localhost:3000/users/sign_in
-#devise_for :admins, path: 'admins'
-# eg. http://localhost:3000/admins/sign_in
-
-#devise_for :users, path: 'users', controllers: { sessions: "users/sessions" }
-#devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions" }
-
-#ROUTING BROWSER PATH REQUESTS TO THE CONTROLLERS AND THEIR METHODS FOR THE CONTROLLERS TO ACTIVATE VIEWS
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
 
-   
   
 		match ':controller(/:action(/:id))', :via => :get
-
-
-get '/teacher' => 'users#teacher'
-  get '/parent' => 'users#parent'
-		get '/principal' => 'users#principal'
-		  get '/teacher' => 'users#teacher'
-		get '/student' => 'users#student'
-
 
 
 end
