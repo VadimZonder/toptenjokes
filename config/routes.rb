08fb106843d3683bbf5d1fb0e92bcb0d527bb087
Vadim <vadimmalakhovski@hotmail.com>
2018-04-27 07:13:29 +0000

    heroku final

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,38 @@
 Rails.application.routes.draw do
     resources :students do 
-    resources :results
+        resources :results
     end
-  resources :results
-  resources :students
-resources :users
-resources :pfilters
-resources :lfilters
- resources :parents
+    
+    resources :results
+    resources :students
+    resources :users
+    resources :pfilters
+    resources :lfilters
+    resources :parents
 
-post '/search' => 'students#search'
+    post '/search' => 'students#search'
 
-  get 'sessions/new'
+    get 'sessions/new'
 
-  get 'sessions/create'
+    get 'sessions/create'
 
-  get 'sessions/destroy'
+    get 'sessions/destroy'
 
 
   
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#create', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'page/index'
+    resources :sessions, only: [:new, :create, :destroy]
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#create', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
+    get 'page/index'
+    
     # You can have the root of your site routed with "root"
-  #ROOT PAGE IS THE STATI_COTROLLER'S INDEX METHOD WHICH WILL BE RENDERED IN VIEWS
-   root 'static_pages#home'
+    #ROOT PAGE IS THE STATI_COTROLLER'S INDEX METHOD WHICH WILL BE RENDERED IN VIEWS
+     root 'static_pages#home'
      get '/about' => 'static_pages#about'
  
 
-
-  
-		match ':controller(/:action(/:id))', :via => :get
+	match ':controller(/:action(/:id))', :via => :get
 
 
 end
