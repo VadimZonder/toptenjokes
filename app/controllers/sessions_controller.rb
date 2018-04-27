aad987983ba5f37ec495a15f0a48c602b3e33880
Vadim <vadimmalakhovski@hotmail.com>
2018-04-27 16:30:36 +0000

    heroku final

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,14 @@
 class SessionsController < ApplicationController
- # skip_before_filter :login_first
-#when login link is pressed the new method is triggered
+  # skip_before_filter :login_first
+  #when login link is pressed the new method is triggered
   def new
-   # @user = User.find params[:id]
-      
   end
   
-    def error
-      
-     
- 
-    end
+  def error
+  end
   
   
-#whenever the login button is pressed after entering email and password this method is triggerd  
+   #whenever the login button is pressed after entering email and password this method is triggerd  
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
@@ -22,22 +17,21 @@ class SessionsController < ApplicationController
       redirect_to root_url #, notice: 'Logged in!'
  
     else
-
-     #flash.keep[:notice]="This message will persist"
-      #redirect_to login_url, notice: 'Logged in!'
-    #flash[:notice] = 'Please make alsways sure that the login details are correct'
-      #render :new
+        #flash.keep[:notice]="This message will persist"
+        #redirect_to login_url, notice: 'Logged in!'
+        #flash[:notice] = 'Please make alsways sure that the login details are correct'
+        #render :new
       
        url = request.path_info
     if url.include?('session')
     flash[:notice] = 'Please make alsways sure that the login details are correct'
   else
     
-  end
+    end
     end
   end
+  
   def destroy
-
     session[:user_id] = nil
     redirect_to root_url #, notice: 'Logged out!'
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,51 +1,18 @@
 class StaticPagesController < ActionController::Base
-
-#include is inheritence to get the contents of the helper. 
-#This is to avoid writing repetative code and is therefore achieveing DRYness
-include UsersHelper
+ #include is inheritence to get the contents of the helper. 
+ #This is to avoid writing repetative code and is therefore achieveing DRYness
+ include UsersHelper
 
    
   #if index is requeste from the browser and it is routed in routs it will run index in views
 
   def home
-
-  
-       #getting a firstname and surname to prefill in the _form
- # @user_first_name= current_user
- # @user_last_name= current_user
- # @user_email= current_user
-  #@user_id= current_user 
-   
-   #def current_user
-   # User.where(id: session[:user_id]).first
-   #end
-
-  
-
- # @usr = User.find_by(id: session[:user_id])
- # @usr2 = User.where(id: session[:user_id]).first
- # @usr3 = User.find_by(id: session[:user_id]).email
- 
-    
-   
-   
-  
    @time = Time.now.strftime("%B %e, %Y at %I:%M %p")
-    
-
-   #@user = current_user
-
   end
-  
 
-  
-#(if from browser /sample is called)
-def about
-
-end
- #(when in views var will be called)
-
-
+ def about
+ 
+ end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
